2a3a13f8836c008a9fbd4acf9167f67741f51de3
rrenatovich <rsalimzyanov@yahoo.com>
2024-10-16 15:04:47 +0700

    fixed

--- Model.cs
+++ Model.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 namespace Imitation_of_Stormy_Activity_ISA_console
 {
-<<<<<<< HEAD
     internal class Model
     {
         private int numberOfNodes = 2;
@@ -19,7 +18,7 @@ namespace Imitation_of_Stormy_Activity_ISA_console
             nodes = new List<Node>();
             for (int i = 0; i < numberOfNodes; i++)
             {
-                nodes.Add(new Node(i+1));
+                nodes.Add(new Node(i + 1));
             }
             matrixTransit = new double[numberOfNodes + 1][];
             matrixTransit[0] = new double[] { 0, 0.5, 0.5, };
@@ -39,39 +38,20 @@ namespace Imitation_of_Stormy_Activity_ISA_console
 
                 if (p <= 0)
                 {
-                    nodes[i-1].AddTask();
+                    nodes[i - 1].AddTask();
                     /*Console.WriteLine(i);*/
                     break;
                 }
             }
         }
 
-        public void GetInfo() 
+        public void GetInfo()
         {
-            for (int i = 0;i < numberOfNodes; i++)
+            for (int i = 0; i < numberOfNodes; i++)
             {
                 Console.WriteLine(nodes[i]._currentNumberTask);
             }
         }
-=======
-    internal class Node
-    {
-        List<Request> requests { get; set; } = null!;
-        private int _currentNumberTask = 0;
-        Request currentTask { get; set; } = null!;
-        public int id;
-        public Node(int id)
-        {
-            this.id = id;
-        }
 
-        public Request FindCurrentTask()
-        {
-            currentTask = requests.MinBy(p => p.time);
-            return currentTask;
-        }
-
->>>>>>> c317897cee21611c9fca54d2e08a13b360d4bd02
     }
-
 }
--- Program.cs
+++ Program.cs
@@ -3,16 +3,11 @@
 Model model = new();
 Console.WriteLine(model);
 
-<<<<<<< HEAD
+
 for (int i = 0; i < 1000; i++)
 {
     model.GetInputTask();
 }
 Console.WriteLine(' ');
 model.GetInfo();
-=======
-for (int i = 0; i < 10; i++)
-{
-    model.GetInputTask();
-}
->>>>>>> c317897cee21611c9fca54d2e08a13b360d4bd02
+
